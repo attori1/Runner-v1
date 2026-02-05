8644b5989e9e30076f2648d6e362f512c860eda3
attori1 <younes.mohamed.ziane@gmail.com>
2026-02-05 16:06:27 +0100

    Le runner est dessiné

--- game.js
+++ game.js
@@ -1,15 +1,6 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d')
 
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // game stuff later
-
-    requestAnimationFrame(gameLoop);
-}
-
 const player = {
     x: 50,
     y: canvas.height - 60,
@@ -20,3 +11,29 @@ const player = {
     isJumping: false
 };
 
+// dessine le joueur comme un rectangle rouge
+function drawPlayer(){
+    ctx.fillStyle ='#FF0000';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function updatePlayer() {
+    player.y += player.velocityY;
+    player.velocityY += player.gravity;
+
+    // pour empecher le joueur de sortir du canvas
+    if(player.y + player.height >= canvas.height - 10) {
+        player.y = canvas.height - player.height - 10;
+        player.isJumping = false;
+        player.velocityY = 0;
+    }
+}
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    updatePlayer();
+    drawPlayer();
+
+    requestAnimationFrame(gameLoop);
+}
